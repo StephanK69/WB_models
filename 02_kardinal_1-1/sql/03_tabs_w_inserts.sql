34bea8b3416ffb46985c59a2bb4aa9d08f33eac0
Peter <schmidt.multimedia@gmail.com>
2022-02-21 08:26:41 +0100

    update

--- 02_kardinal_1-1/sql/03_tabs_w_inserts.sql
+++ 02_kardinal_1-1/sql/03_tabs_w_inserts.sql
@@ -28,7 +28,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`servants` (
   `yrs_served` INT NOT NULL,
   `cats_id` INT NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_servants_cats_idx` (`cats_id` ASC) ,
+  INDEX `fk_servants_cats_idx` (`cats_id` ASC),
   CONSTRAINT `fk_servants_cats`
     FOREIGN KEY (`cats_id`)
     REFERENCES `mydb`.`cats` (`id`)
