c85151ee21a065517eb1c31ee76ead321da1082c
Peter <schmidt.multimedia@gmail.com>
2022-02-22 11:45:09 +0100

    03 / INNER JOIN / 1:n / updates

--- 03_kardinal_1-n/sql/04_selects_1-n.sql
+++ 03_kardinal_1-n/sql/04_selects_1-n.sql
@@ -10,3 +10,24 @@ SELECT
 FROM mydb.cats INNER JOIN mydb.kittens
 ON mydb.cats.id  = mydb.kittens.cats_id
 ;
+
+-- Inner Join 2 / Verwandschaft
+-- "X ist die Mutter von Y"
+SELECT 
+	cat_name AS Mutter,
+    kitten_name AS Kind,
+    concat(cat_name," ist die Mutter von ", kitten_name, ".") AS Verwandschaft
+FROM mydb.cats INNER JOIN mydb.kittens
+ON mydb.cats.id  = mydb.kittens.cats_id
+;
+
+-- Inner Join 3 / Wieviel Kinder haben die Katzen?
+SELECT 
+	cat_name AS Katze, -- nicht aggregiert / organisch
+    count(cat_name) AS Kinderzahl -- aggregiert / f()
+FROM mydb.cats INNER JOIN mydb.kittens
+ON mydb.cats.id  = mydb.kittens.cats_id
+GROUP BY cat_name -- Wichtig bei Kombi Aggr./Nicht-Aggr.
+;
+
+
